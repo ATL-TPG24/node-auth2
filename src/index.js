6e81b260c86ca173240e22e7091b31214f2cb11c
Tony <64141019+ATL-TPG24@users.noreply.github.com>
2024-03-18 21:21:26 -0400

    adding mongoDB

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
+require('dotenv').config();
 const express = require('express');
+const connectToMongo = require('./config/database');  // Adjust path as necessary
+const userRoutes = require('./routers/userRouter');    // Example route
+
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-const userRouter = require('./routers/userRouter');
+const startServer = async () => {
+    try {
+      await connectToMongo(); // Establish MongoDB connection
+      app.listen(port, () => console.log(`Server running on port ${port}`));
+    } catch (error) {
+      console.error('Unable to connect to MongoDB', error);
+      process.exit(1);
+    }
+  };
 
-// app.get('/', (req, res) => {
-//   res.send('Hello World!!! Nascar!');
-// });
-app.use('/', userRouter);
-app.use('/items', userRouter);
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use('/', userRoutes); // Use user routes
 
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
+startServer();
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,31 +1,12 @@
 const express = require('express');
-const app = express();
 const router = express.Router();
 
-// Middleware to parse JSON bodies
-app.use(express.json());
-
-// Basic route for GET request
-app.get('/', (req, res) => {
-  res.send('Hello World! Nascar!');
+// Define routes specific to users
+router.get('/', (req, res) => {
+  res.send('Hello World from User Router!');
 });
 
-// REST API routes
-// GET, POST, PUT, DELETE operations might correspond to read, create, update, delete operations.
+// Add more user-specific routes as needed here
 
-// Example: GET request to retrieve a list of items
-app.get('/items', (req, res) => {
-  res.status(200).json({ message: 'Getting all items' });
-});
-
-// Example: POST request to add a new item
-app.post('/items', (req, res) => {
-  // Add logic to add item
-  res.status(201).json({ message: 'Item added' });
-});
-
-// Replace '3000' with the port number you want your server to listen on.
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
-
+// Export the router at the end
 module.exports = router;
